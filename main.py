612c18a35d79d516ce0b093d908cb0439d4fbc78
3511 <maxym.taranukha@gmail.com>
2026-03-12 19:03:47 +0200

    s

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hellohgjhg world")
+class Human:
+    height = 180
+    name1 = "KATE"
+    name2 = "DRAKE"
+class Student(Human):
+    pass
+class Worker(Human):
+    pass
+
+nick = Student()
+jon = Worker()
+print(nick.name1)
+print(jon.name2)
